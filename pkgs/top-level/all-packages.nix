e9ef8e3eb0730d2870fc15c5823d8410a8d5595b
Frederik Rietdijk <fridh@fridh.nl>
2017-04-23 14:47:58 +0200

    Julia: generate packages.json

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -6802,6 +6802,9 @@ with pkgs;
 
   julia = julia_06;
 
+  juliaLang = callPackage ../development/compilers/julia/julia-lang.nix { };
+  juliaPackages = callPackage ../development/compilers/julia/packages/packages.nix { };
+
   jwasm =  callPackage ../development/compilers/jwasm { };
 
   kotlin = callPackage ../development/compilers/kotlin { };
